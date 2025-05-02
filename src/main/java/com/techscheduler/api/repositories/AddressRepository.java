92376373bc9f6cd744f0fea85b11b4ae66cd63e2
Capex <you@example.com>
2025-05-01 21:16:15 -0300

    Add repositories

---
+++ src/main/java/com/techscheduler/api/repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.techscheduler.api.repositories;
+
+
+import com.techscheduler.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/techscheduler/api/repositories/CouponRepository.java
@@ -0,0 +1,8 @@
+package com.techscheduler.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository <Coupon, UUID> {
+}
